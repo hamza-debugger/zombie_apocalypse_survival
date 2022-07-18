0bd45c4f3d7491c3a0fc137f266960475fc29cb6
hamza-debugger <hamza.razzaq@devsinc.com>
2022-07-18 17:11:05 +0500

    Fix

--- config/prod.exs
+++ config/prod.exs
@@ -28,13 +28,13 @@ config :logger, level: :info
       ...,
       url: [scheme: "https", host: "floating-spire-31322.herokuapp.com", port: 443],
       force_ssl: [rewrite_on: [:x_forwarded_proto]],
-      https: [
-        ...,
-        port: 443,
-        cipher_suite: :strong,
-        keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-        certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-      ]
+      # https: [
+      #   ...,
+      #   port: 443,
+      #   cipher_suite: :strong,
+      #   keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+      #   certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
+      # ]
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
